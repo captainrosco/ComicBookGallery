0adf8a3662f5004fd31446380d86de95e05b9816
Daron Short <daron.short@hotmail.com>
2018-03-04 12:25:57 -0600

    Added ComicBookRepository

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -17,22 +17,9 @@ namespace ComicBookGallery.Controllers {
 
         public ActionResult Detail() {
 
-            var comicBook = new ComicBook() {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                Favorite = true,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artists[] {
-                    new Artists() {Name ="Script", Role ="Dan Slott"},
-                    new Artists() {Name ="Penciles", Role ="Humberto Ramos"},
-                    new Artists() {Name ="Inks", Role ="Victor Olazaba"},
-                    new Artists() {Name ="Colors", Role ="Edgar Delgago"},
-                    new Artists() {Name ="Letters", Role ="Chris Eliopoulos"},
-                }
-                
-            };
+            
 
-            return View(comicBook);
+            return View();
         }
     }
 }
