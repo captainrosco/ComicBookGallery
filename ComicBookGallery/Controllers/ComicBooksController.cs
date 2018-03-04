28fc2ee414c975e4c0a5b8e2fd1c76532cae66d6
Daron Short <daron.short@hotmail.com>
2018-03-04 11:50:15 -0600

    Updated Controller to Pass Content

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,17 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ComicBookGallery.Controllers
-{
-    public class ComicBooksController : Controller
-    {
-        // GET: ComicBooks
-        public ActionResult Index()
-        {
+namespace ComicBookGallery.Controllers {
+    public class ComicBooksController : Controller {
+
+       
+
+    // GET: ComicBooks
+    public ActionResult Index() {
             return View();
         }
 
         public ActionResult Detail() {
+            ViewBag.seriesTitle = "The Amazing Spider-Man";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.artists = new string[]
+            {
+        "Script: Dan Slott",
+        "Pencils: Humberto Ramos",
+        "Inks: Victor Olazaba",
+        "Colors: Edgar Delgado",
+        "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
     }
